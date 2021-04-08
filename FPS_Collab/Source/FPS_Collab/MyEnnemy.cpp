90487f1425a46fe6be17cfa51a8e71478a9bc09a
Paulux92500 <paul.ray92500@gmail.com>
2021-04-08 10:42:14 +0200

    Ennemy & import avion / helice

---
+++ FPS_Collab/Source/FPS_Collab/MyEnnemy.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyEnnemy.h"
+#include "FPS_CollabCharacter.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "AIController.h"
+
+// Sets default values
+AMyEnnemy::AMyEnnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyEnnemy::MoveToTarget(AFPS_CollabCharacter* Target)
+{
+    if (pController)
+    {
+        FAIMoveRequest MoveRequest;
+        MoveRequest.SetGoalActor(Target);
+
+        // (Stopping distance in unity)
+        MoveRequest.SetAcceptanceRadius(25.0f);
+
+        FNavPathSharedPtr NavPath;
+
+        pController->MoveTo(MoveRequest, &NavPath);
+    }
+}
+
+// Called when the game starts or when spawned
+void AMyEnnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	pController = Cast<AAIController>(GetController());
+	character = Cast<AFPS_CollabCharacter>(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
+}
+
+// Called every frame
+void AMyEnnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MoveToTarget(character);
+}
+
+// Called to bind functionality to input
+void AMyEnnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ FPS_Collab/Source/FPS_Collab/MyEnnemy.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyEnnemy.generated.h"
+
+class AFPS_CollabCharacter;
+
+UCLASS()
+class FPS_COLLAB_API AMyEnnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyEnnemy();
+
+	AFPS_CollabCharacter* character;
+
+	void MoveToTarget(AFPS_CollabCharacter* Target);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	class AAIController* pController;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
